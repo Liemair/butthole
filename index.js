1254db1f45a3a69d6b404b1b2f550d6247c5d4fc
liemair <pachump001@gmail.com>
2020-08-23 15:22:08 -0700

    idiot/cool changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const cool = require('cool-ascii-faces');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -7,8 +7,8 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  // todo: fix this later
-  //.get('/cool', (req, res) => res.send(cool()))
+  // todo: fix this later "//" this is commented out.
+  .get('/cool', (req, res) => res.send(cool()))
   .get('/idiot', (req, res) => res.send("spencer's  a fucking idiot"))
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
