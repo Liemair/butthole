4ad2a8cfd866ac3528d966f58e06bc859d44d191
liemair <pachump001@gmail.com>
2020-08-23 15:47:56 -0700

    /Times added

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ express()
   .set('view engine', 'ejs')
   // todo: fix this later "//" this is commented out.
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => res.send(showTimes()))
   .get('/idiot', (req, res) => res.send("spencer's  a fucking idiot"))
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  showTimes = () => {
+    let result = '';
+    const times = process.env.TIMES || 5;
+    for (i = 0; i < times; i++) {
+      result += i + ' ';
+    }
+    return result;
+  }
